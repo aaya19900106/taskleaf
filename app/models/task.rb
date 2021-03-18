30f1ad5ffd7db44c15d6fd4af0cb181273c07a3c
aaya19900106 <katsuya@startup-technology.com>
2021-03-19 03:31:11 +0900

    nilを置き換えるコード削除

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,6 @@
 class Task < ApplicationRecord
-  before_validation :set_nameless_name
 
-  validates :name,presence: true
-  validates :name, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
   belongs_to :user
@@ -10,11 +8,6 @@ class Task < ApplicationRecord
   scope :recent, -> { order(created_at: :desc) }
 
   private
-
-  def set_nameless_name
-    self.name = '名前なし' if name.blank?
-  end
-
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含める事はできないぞ！') if name&.include?(',')
